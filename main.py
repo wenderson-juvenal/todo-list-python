b2736744cfbe19bcec1e5de9c61f6356537b0089
wenderson-juvenal <wendersjuv@gmail.com>
2024-01-12 20:26:36 -0300

    criando labels dinamicamente para cada task numa lista

--- main.py
+++ main.py
@@ -9,16 +9,27 @@ from kivy.uix.textinput import TextInput
 class HomeScreen(Screen):
     def __init__(self, **kwargs):
         super(HomeScreen, self).__init__(**kwargs)
+    
 
-class SecondScreen(Screen):
+class addTaskScreen(Screen):
     def __init__(self, **kwargs):
-        super(SecondScreen, self).__init__(**kwargs)
+        super(addTaskScreen, self).__init__(**kwargs)
 
 class MainApp(App):
 	def build(self):
 		sm = ScreenManager()
 		sm.add_widget(HomeScreen(name='home'))
-		sm.add_widget(SecondScreen(name='second'))
+		sm.add_widget(addTaskScreen(name='addTask'))
+          
+		taskList = ['Item 1', 'Item 2', 'Item 3']
+		
+		homeScreen = sm.get_screen('home')
+		tasksLayout = homeScreen.ids.tasks
+
+		for item in range(100):
+			label = Label(text=str(item), size_hint_y=None, height=40)
+			tasksLayout.add_widget(label)
+
 		return sm
 
 randomApp = MainApp()
