2b0cd3a56a56a34b2f7ec2359c8de582e59b44d8
root <root@user2225>
2024-01-16 20:22:45 -0300

    commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+
 import kivy
+from kivy import Config
+Config.set('graphics', 'gl_version', '1.1')
+Config.set('graphics', 'minimum_gl_version', '1.1')
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -7,9 +12,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 
 class HomeScreen(Screen):
-    def __init__(self, **kwargs):
-        super(HomeScreen, self).__init__(**kwargs)
-    
+	def __init__(self, **kwargs):
+		super(HomeScreen, self).__init__(**kwargs)
 
 class addTaskScreen(Screen):
     def __init__(self, **kwargs):
@@ -21,14 +25,6 @@ class MainApp(App):
 		sm.add_widget(HomeScreen(name='home'))
 		sm.add_widget(addTaskScreen(name='addTask'))
           
-		taskList = ['Item 1', 'Item 2', 'Item 3']
-		
-		homeScreen = sm.get_screen('home')
-		tasksLayout = homeScreen.ids.tasks
-
-		for item in range(100):
-			label = Label(text=str(item), size_hint_y=None, height=40)
-			tasksLayout.add_widget(label)
 
 		return sm
 
